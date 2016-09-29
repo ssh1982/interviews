b91de052c39ed148de927067a2b10f0d6253ab61
ssh1982 <mkhadanovich@gmail.com>
2016-09-28 21:05:36 -0400

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -1,20 +1,45 @@
 package com.elsevier.education;
 
 /**
-
-TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
-
-*/
+ * TODO: Turn the "Singleton" class into an actual singleton. The main() method
+ * should still call .doSomething().
+ */
 public class Exercise5 {
-	
+
 	public static class Singleton {
+		
+		private static Singleton instance = null;
+		
+		/*
+		 * Declare private default constructor to ensure that only 
+		 * getInstance method can be used to create new instance
+		 */
+		private Singleton() {
+			
+		}
+		/*
+		 * Creates new instance if it doesn't exists
+		 * and returns the only instance 
+		 */
+		public static Singleton getInstance() {
+			if (Singleton.instance == null) {
+				Singleton.instance = new Singleton();
+			}
+			return instance;
+		}
+		
 		public void doSomeThing() {
 			System.out.println("Doing something....");
-	    }
+		}
 	}
-		
-	public static void main(String a[]){
+
+	public static void main(String a[]) {
 		Singleton st = new Singleton();
 		st.doSomeThing();
+		/*
+		 * Singleton object should be created as follows
+		 */
+		Singleton st1 = Singleton.getInstance();
+		st1.doSomeThing();
 	}
 }
